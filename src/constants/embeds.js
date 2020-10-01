8205e8592840675d54162aced07d45fe97ce339e
Promise <glelud@gmail.com>
2020-10-01 23:43:51 +0200

    Add changelog

--- src/constants/embeds.js
+++ src/constants/embeds.js
@@ -18,6 +18,18 @@ module.exports.panelEmbeds = [
     color: 0xE74C3C
   },
   {
+    title: "Changelog October",
+    description: [
+      "[•](https://github.com/biaw/the-impostor/issues?q=is%3Aissue+is%3Aclosed+-label%3Awontfix) Made a changelog that will reset monthly. (Yeah, you're looking at it!)",
+      "[•](https://github.com/biaw/the-impostor/issues/3) Introduced a new neat help command with detailed description of each command.",
+      "[•](https://github.com/biaw/the-impostor/issues/6) The bot will no longer randomly refresh the panel. It will now only refresh when starting the bot and when refreshing it manually.",
+      "[•](https://github.com/biaw/the-impostor/issues/7) Introduced a new toggle system. You now only have one button instead of two so you don't accidentally misclick.",
+      "[•](https://github.com/biaw/the-impostor/issues/8) The bot will now show how many games are being played with the bot in its presence.",
+      "[•](https://github.com/biaw/the-impostor/issues/15) When leaving a game VC, the bot will try and unmute/undeafen you. Also, when joining a game VC, the bot will treat you as a ghost and mute you if there's a discussion."
+    ].join("\n\n"),
+    color: 0x1ABC9C
+  },
+  {
     title: "Host Actions",
     description: [
       "🔘 **SWITCH:** Switch between in-game and discussion.",
