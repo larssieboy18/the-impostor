33f0543e05633eb52d2ac4f2f316c816f1209159
Promise <glelud@gmail.com>
2020-10-01 22:26:29 +0200

    Introducing new toggle system

--- src/handlers/hosting.js
+++ src/handlers/hosting.js
@@ -1,4 +1,4 @@
-const actionPanels = new Map(); // guild ID: action panel message ID
+const actionPanels = new Map(), gameStates = new Map(); // guild ID: action panel message ID
 
 const { panelEmbeds } = require("../constants/index.js")
 
@@ -35,20 +35,39 @@ module.exports.configure = (client, db) => { // on startup
       let vc = member.voice.channel;
       if (!vc || !vc.parent || vc.parent.id !== category || !vc.permissionsFor(user).has("MUTE_MEMBERS")) return reaction.users.remove(user); // they're not in a game VC, or they don't have host perms in the game VC, so we cancel
 
-      if (reaction.emoji.name == "🔇") {
-        let alive = vc.members.filter(member => !member.voice.serverMute), dead = vc.members.filter(member => member.voice.serverMute);
-        await Promise.all(alive.map(member => member.edit({ mute: false, deaf: true })))
-        await Promise.all(dead.map(member => member.edit({ mute: false, deaf: false })))
-      } else if (reaction.emoji.name == "🔊") {
-        let alive = vc.members.filter(member => member.voice.serverDeaf), dead = vc.members.filter(member => !member.voice.serverDeaf);
-        await Promise.all(dead.map(member => member.edit({ mute: true, deaf: false })))
-        await Promise.all(alive.map(member => member.edit({ mute: false, deaf: false })))
+      if (reaction.emoji.name == "🔘") {
+        let gameState = gameStates.get(vc.id);
+        if (!gameState) { // try and find out what the game state is. this is most commonly used when the bot restarts and has no idea whether it's mid-game or not.
+          await vc.fetch();
+          let voiceStates = vc.members.map(member => {
+            if (member.voice.serverMute) return "mute";
+            if (member.voice.serverDeaf) return "deaf";
+            return null;
+          }).sort(s => s), mostCommon = voiceStates.sort((a, b) => voiceStates.filter(s => s == a).length - voiceStates.filter(s => s == b).length).pop();
+          if (mostCommon == "deaf") gameState = "in-game";
+          else gameState = "discussion";
+        }
+
+        if (gameState == "in-game") { // we want to mute the dead ones and undeafen the alive ones
+          gameStates.set(vc.id, "discussion")
+          let alive = vc.members.filter(member => member.voice.serverDeaf), dead = vc.members.filter(member => !member.voice.serverDeaf);
+          await Promise.all(dead.map(member => member.edit({ mute: true, deaf: false })))
+          await Promise.all(alive.map(member => member.edit({ mute: false, deaf: false })))
+        } else { // discussion or game-over; we want to deafen the alive ones and unmute the dead ones
+          gameStates.set(vc.id, "in-game")
+          let alive = vc.members.filter(member => !member.voice.serverMute), dead = vc.members.filter(member => member.voice.serverMute);
+          await Promise.all(alive.map(member => member.edit({ mute: false, deaf: true })))
+          await Promise.all(dead.map(member => member.edit({ mute: false, deaf: false })))
+        }//🔰
       } else if (reaction.emoji.name == "♻️") {
+        gameStates.set(vc.id, "game-over")
         await Promise.all(vc.members.filter(member => member.voice.serverMute || member.voice.serverDeaf).map(member => member.edit({ mute: false, deaf: false })))
       }
       reaction.users.remove(user)
     }
   })
+
+  client.on("channelDelete", channel => gameStates.delete(channel.id))
 }
 
 module.exports.refreshPanel = async hChannel => {
@@ -64,9 +83,7 @@ module.exports.refreshPanel = async hChannel => {
     let lastPanel;
     for (const embed of panelEmbeds) lastPanel = await hChannel.send({ embed });
     actionPanels.set(hChannel.guild.id, lastPanel.id)
-    await lastPanel.react("🔇");
-    await pause(1000);
-    await lastPanel.react("🔊");
+    await lastPanel.react("🔘");
     await pause(1000);
     await lastPanel.react("♻️");
     return true; // done!
